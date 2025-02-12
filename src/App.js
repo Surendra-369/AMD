7345499004fa647d33a23f2fb1c5210cc7aef999
Surendra-369 <81153933+surendra-malli@users.noreply.github.com>
2025-02-12 18:49:56 +0530

    mqtt context added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import MqttClient from "./components/Mqtt/MqttClient";
 import { VisibilityProvider } from "./components/VisibilityProvider";
+import { MqttProvider } from "./MqttContext";
 
 function App() {
   return (
     <>
     {/* <MqttClient/> */}
+    <MqttProvider>
+      
+   
     <VisibilityProvider>
       <Header />
       <Home />
       <MqttClient/>
       <Footer />
       </VisibilityProvider>
+      </MqttProvider>
     </>
   );
 }
---
+++ src/components/Mqtt/MqttContext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+// Create the context
+const MqttContext = createContext();
+
+// Custom hook for consuming context
+export const useMqtt = () => useContext(MqttContext);
+
+// Context provider component
+export const MqttProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+
+  return (
+    <MqttContext.Provider value={{ messages, setMessages }}>
+      {children}
+    </MqttContext.Provider>
+  );
+};
