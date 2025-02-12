6857739d670b1adf4dbd8ca7ea01f55b801b55d5
sairam843 <sairam8431@gmail.com>
2025-02-12 12:45:23 +0530

    ram changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import MqttClient from "./components/Mqtt/MqttClient";
 import { VisibilityProvider } from "./components/VisibilityProvider";
 
 function App() {
   return (
     <>
-    {/* <MqttClient/> */}
     <VisibilityProvider>
       <Header />
       <Home />
-      <MqttClient/>
       <Footer />
       </VisibilityProvider>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,51 @@
-import React from "react";
-
-const Header = ()=>{
-    return (
-        <div className="header-list">
-            <div className="list-items">
-                <h1>
-                    AMD
-                </h1>
-                <h1>
-                    Icon
-                </h1>
-            </div>
-        </div>
-    )
-}
-export default Header
+import React from 'react';
+ 
+const Header = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        padding: '10px',  // Reduced from 20px
+        height: '60px'    // Added fixed height
+      }}
+      className="header-list"
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%',
+          position: 'relative',
+          height: '100%'  // Fill the parent height
+        }}
+        className="list-items"
+      >
+        <h1
+          style={{
+            fontSize: '1.5rem',  // Reduced from 2rem
+            fontWeight: 'bold',
+            position: 'absolute',
+            left: '20px',
+            margin: 0  // Remove default margins
+          }}
+        >
+          AMD
+        </h1>
+        <h2
+          style={{
+            fontSize: '1.2rem',  // Reduced from 1.5rem
+            fontWeight: 'normal',
+            width: '100%',
+            textAlign: 'center',
+            padding: '0 100px',
+            margin: 0  // Remove default margins
+          }}
+        >
+          Efficient Performance and Optimization with AMD EPYC™
+        </h2>
+      </div>
+    </div>
+  );
+};
+ 
+export default Header;
